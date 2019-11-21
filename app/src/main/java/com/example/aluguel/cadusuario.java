1f31c721a04e0e3b6bfd550a9113428030811fab
GiovanaNobre <gionobre4@gmail.com>
2019-11-21 09:46:46 -0300

    -teste

--- app/src/main/java/com/example/aluguel/cadusuario.java
+++ app/src/main/java/com/example/aluguel/cadusuario.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class cadusuario extends AppCompatActivity {
-//
+////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
